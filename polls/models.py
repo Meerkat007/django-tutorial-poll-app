94b703f6f6d0bad7246c48ce950e6170705bae69
Yuan <yuanji.yj@gmail.com>
2016-06-19 18:15:37 -0400

    Added models

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# The single, definitive source of truth about my data.
+# Contains the essential fields and behaviors of the data I'm storing
+
+#class subclass models.Model
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
